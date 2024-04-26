8e39f9f7c72445cab8414c7cc1da69105bdd3de5
Negar Morovatian <97600041+Negmor@users.noreply.github.com>
2024-04-26 20:52:45 +0330

    Authentication

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from rest_framework.authtoken import views as token_view
 urlpatterns = [
 
     path("blog", views.hello_world),
@@ -12,5 +12,7 @@ urlpatterns = [
     path("article/add", views.ArticleaddView.as_view()),
     path("article/update/<int:pk>", views.ArticleUpdateView.as_view()),
     path("check", views.CheckToken.as_view()),
+    path("login",  token_view.obtain_auth_token),
 
 ]
+
